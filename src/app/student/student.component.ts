1aac33874652cde780179908209ecac1ac0e50ad
demeurea <alexandre.demeure@univ-grenoble-alpes.fr>
2020-11-06 10:35:02 +0100

    update text and style

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -16,4 +16,18 @@ export class StudentComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getBackground(): string {
+    let color: 'red' | 'yellow' | 'green' = 'red';
+    if (this.data.note >= 10) {
+      if (this.data.note > 14) {
+        color = 'green';
+      } else {
+        color = 'yellow';
+      }
+    } else {
+      color = 'red';
+    }
+    const percent = `${this.data.note * 100 / 20}%`;
+    return `linear-gradient(90deg, ${color} ${percent}, white ${percent}, white)`;
+  }
 }
